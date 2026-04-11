e21d36ae7111a714e99be11b9fc34ff8b602d381
Gauri A.  Hulle <gauri.hulle@gmail.com>
2026-04-11 21:32:21 +0530

    Assignment-8.c

--- Assignment-8.c
+++ Assignment-8.c
@@ -1,8 +1,8 @@
-//Assignment 8:
+/*Assignment 8:
 To accept a student's five course marks and compute his/her result. Student is passing if he/she scores marks equal to 
 and above 40 in each course. If student scores aggregate greater than 75%, then the grade is distinguished.
 If aggregate is 60>= and <75 then the grade of first division. If aggregate is 50>= and <60, then the grade is second division. 
-If aggregate is 40>= and <50, then the grade is third division.
+If aggregate is 40>= and <50, then the grade is third division.*/
 
 
 #include<stdio.h>
@@ -55,7 +55,7 @@ int main()
 
 }
 
-*OUTPUT:
+/*OUTPUT:
 Enter your marks in 5 courses(out of 100):=
 Marks in Mathematics:=90
 Marks in Physics:=90
@@ -65,4 +65,4 @@ Marks in Computer Science:=99
 
 TOTAL PERCENTAGE:= 90.80
 PASS
-Grade= DISTINGUISHED
+Grade= DISTINGUISHED*/
