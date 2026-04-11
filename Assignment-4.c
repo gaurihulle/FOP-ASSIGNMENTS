37d8a58d1b46eff359307db80735e792b53e054a
Gauri A.  Hulle <gauri.hulle@gmail.com>
2026-04-11 21:29:37 +0530

    Assignment-4.c

--- Assignment-4.c
+++ Assignment-4.c
@@ -1,7 +1,7 @@
-//Assignment 4:
+/*Assignment 4:
 Write a C Program to calculate the salary of an employee given his basic pay (taken as input from the user). 
 Calculate gross salary of employee. Let HRA be 10 % of basic pay and TA be 5% of basic pay. 
-Let employees pay professional tax as 2% of total salary. Calculate net salary payable after deductions.
+Let employees pay professional tax as 2% of total salary. Calculate net salary payable after deductions.*/
 
 
 #include<stdio.h>
@@ -23,7 +23,9 @@ int main()
 	printf("\n Net salary (after deductions):= %.2f", net_sal);
 	return 0;
 };
-*OUTPUT:=
+
+
+/*OUTPUT:=
 Enter basic pay:=5000000
 
  Basic pay:= 5000000.00
@@ -31,4 +33,4 @@ Enter basic pay:=5000000
  TA:= 250000.00
  Gross salary:= 5750000.00
  Proffesional tax to be paid:= 115000.00
- Net salary (after deductions):= 5635000.00
+ Net salary (after deductions):= 5635000.00*/
